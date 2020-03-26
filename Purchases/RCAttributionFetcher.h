04ffcc802af918fee8e7e6a37f0c0fce37123f29
Tom Izaks <tom@bilue.com.au>
2020-03-26 16:35:43 +1100

    Remove all IDFA references

--- Purchases/RCAttributionFetcher.h
+++ Purchases/RCAttributionFetcher.h
@@ -12,13 +12,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RCAttributionFetcher : NSObject
-
-- (nullable NSString *)advertisingIdentifier;
-
 - (nullable NSString *)identifierForVendor;
-
-- (void)adClientAttributionDetailsWithCompletionBlock:(void (^)(NSDictionary<NSString *, NSObject *> * _Nullable attributionDetails, NSError * _Nullable error))completionHandler;
-
 @end
 
 NS_ASSUME_NONNULL_END
--- Purchases/RCAttributionFetcher.m
+++ Purchases/RCAttributionFetcher.m
@@ -17,28 +17,7 @@
 
 @end
 
-@protocol FakeASIdentifierManager <NSObject>
-
-@property (nonnull, nonatomic, readonly) NSUUID *advertisingIdentifier;
-
-+ (instancetype)sharedManager;
-
-@end
-
 @implementation RCAttributionFetcher : NSObject
-
-- (nullable NSString *)advertisingIdentifier
-{
-    if (@available(iOS 6.0, macOS 10.14, *)) {
-        id<FakeASIdentifierManager> asIdentifierManagerClass = (id<FakeASIdentifierManager>)NSClassFromString(@"ASIdentifierManager");
-        if (asIdentifierManagerClass) {
-            return [asIdentifierManagerClass sharedManager].advertisingIdentifier.UUIDString;
-        } else {
-            RCDebugLog(@"AdSupport framework not imported. Attribution data incomplete.");
-        }
-    }
-    return nil;
-}
 
 
 - (nullable NSString *)identifierForVendor
@@ -49,17 +28,6 @@
     }
 #endif
     return nil;
-}
-
-- (void)adClientAttributionDetailsWithCompletionBlock:(void (^)(NSDictionary<NSString *, NSObject *> * _Nullable attributionDetails, NSError * _Nullable error))completionHandler
-{
-#if AD_CLIENT_AVAILABLE
-    id<FakeAdClient> adClientClass = (id<FakeAdClient>)NSClassFromString(@"ADClient");
-    
-    if (adClientClass) {
-        [[adClientClass sharedClient] requestAttributionDetailsWithBlock:completionHandler];
-    }
-#endif
 }
 
 @end
